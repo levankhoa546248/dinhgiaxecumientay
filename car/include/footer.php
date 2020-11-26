ed095526c81111d08f4c5ffaa3d2e4f971ceee90
levankhoa2412 <levankhoa546248@gmail.com>
2020-11-26 10:28:23 +0700

    26112020

--- car/include/footer.php
+++ car/include/footer.php
@@ -70,12 +70,4 @@
     </div>
 </footer>
 </body>
-
-<!-- SCRIPTS -->
-<script src="car/js/jquery.js" type="text/javascript"></script>
-<script src="car/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="car/js/owl.carousel.min.js" type="text/javascript"></script>
-<script src="car/js/smoothscroll.js" type="text/javascript"></script>
-<script src="car/js/custom.js" type="text/javascript"></script>
-<script src="car/js/jquery-3.4.1.js" type="text/javascript"></script>
 </html>
--- car/include/head.php
+++ car/include/head.php
@@ -21,3 +21,11 @@
     <link rel="stylesheet" href="car/css/style.css">
 
 </head>
+
+<!-- SCRIPTS -->
+<script src="car/js/jquery.js" type="text/javascript"></script>
+<script src="car/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="car/js/owl.carousel.min.js" type="text/javascript"></script>
+<script src="car/js/smoothscroll.js" type="text/javascript"></script>
+<script src="car/js/custom.js" type="text/javascript"></script>
+<script src="car/js/jquery-3.4.1.js" type="text/javascript"></script>
