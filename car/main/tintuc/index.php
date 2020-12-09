6b16c4267e95e387d54536be455f55e432938f58
levankhoa2412 <levankhoa546248@gmail.com>
2020-12-09 10:15:10 +0700

    09122020

---
+++ car/main/tintuc/index.php
@@ -0,0 +1,6 @@
+<?php require('car/main/include/head.php'); ?>
+<?php require('car/main/include/menu.php'); ?>
+<?php require('car/main/tintuc/main.php'); ?>
+<?php require('car/main/include/contact.php'); ?>
+<?php require('car/main/include/footer.php'); ?>
+<?php require('car/main/home/script.php'); ?>
